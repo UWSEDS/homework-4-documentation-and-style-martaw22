9e922b4950fa9436d9e72ac57fe3a18e574b72e4
Marta Wolfshorndl <martaw22@gmail.com>
2019-11-06 15:10:22 -0800

    Updated the module doc string and added spaces in the function doc strings.

--- test_dataframe.py
+++ test_dataframe.py
@@ -1,4 +1,4 @@
-'''This is a module for HW3 - Testing'''
+'''This is a module for HW3 - Testing, and updated for style in HW4.'''
 
 import pandas as pd
 import numpy as np
@@ -11,6 +11,7 @@ DF_TEST = pd.DataFrame(data=DF2)
 #Testing item (2) for HW2
 def test_hw2():
     '''This function tests whether the hw2 module works.
+
     It takes no direct imputs, but the test dataframe is defined globally.
     It outputs true if the dataframe passes the requirements.'''
     value = hw2_module.test_create_dataframe(DF_TEST, list(DF_TEST.columns))
@@ -19,6 +20,7 @@ def test_hw2():
 #Testing that all columns have values of the correct type
 def test_correct_type():
     '''This function tests that all columns in the specified dataframe have the correct type.
+
     The type needs to be specified in the function and the test dataframe is defined above globally.
     It returns true if all values in all columns have values of the type specified.'''
     type0 = np.int64
@@ -29,6 +31,7 @@ def test_correct_type():
 #Check for nan values
 def test_nan_check():
     '''This test checks for nan values anywhere in the dataframe.
+
     It uses the global dataframe specified above.
     The test is passed if there are no nan values.'''
     assert not DF_TEST.isnull().values.any()
@@ -37,5 +40,6 @@ def test_nan_check():
 #Check that dataframe has at least one row
 def test_rows():
     '''This test checks that the dataframe has at least one row.
+
     It uses the test dataframe defined globally above.'''
     assert len(DF_TEST.index) >= 1
